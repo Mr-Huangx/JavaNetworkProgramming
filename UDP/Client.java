f6093e0e4c62e4617bbf96a0d486f1803ddad9fc
hx <1148286216@qq.com>
2024-12-28 10:47:03 +0800

    服务器增加一个延迟时间

--- UDP/Client.java
+++ UDP/Client.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Client {
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket datagramSocket = new DatagramSocket();
+        datagramSocket.setSoTimeout(2000);
+
+        //链接服务器和端口
+        datagramSocket.connect(InetAddress.getByName("localhost"), 6666);
+
+        //创建数据缓冲区
+        byte[] data = "Hello ".getBytes(StandardCharsets.UTF_8);
+        DatagramPacket packet = new DatagramPacket(data, data.length);
+
+        // 发送数据
+        datagramSocket.send(packet);
+
+        //接收数据
+        byte[] buffer = new byte[1024];
+        packet = new DatagramPacket(buffer, buffer.length);
+        datagramSocket.receive(packet);
+        String response = new String(packet.getData(), packet.getOffset(), packet.getLength());
+        System.out.println("【服务器返回结果】：" + response);
+
+        datagramSocket.disconnect();
+        datagramSocket.close();
+    }
 }
--- UDP/Server.java
+++ UDP/Server.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Server {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
+
+public class Server {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        DatagramSocket ds = new DatagramSocket(6666);
+        while( true ){
+            // 创建数据缓冲区
+            byte[] buffer = new byte[1024];
+            DatagramPacket datagramPacket = new DatagramPacket(buffer, buffer.length);
+
+            //监听并接收UDP数据包
+            ds.receive(datagramPacket);
+
+            String s = new String(datagramPacket.getData(), datagramPacket.getOffset(), datagramPacket.getLength(), StandardCharsets.UTF_8);
+            System.out.println("【客户端发来数据】：" + s);
+            Thread.sleep(1000);//延迟一秒在返回报文
+
+            //发送该数据
+            byte[] data = "ACK".getBytes(StandardCharsets.UTF_8);
+            datagramPacket.setData(data);
+            ds.send(datagramPacket);
+        }
+    }
 }
